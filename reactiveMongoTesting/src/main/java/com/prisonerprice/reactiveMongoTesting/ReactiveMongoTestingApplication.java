1de02fa56c890116b3586478a9f82a07326c9103
Hangbo <zhzhanghangbo@hotmail.com>
2020-09-02 22:55:30 -0400

    Add reactiveMongoTesting

---
+++ reactiveMongoTesting/src/main/java/com/prisonerprice/reactiveMongoTesting/ReactiveMongoTestingApplication.java
@@ -0,0 +1,13 @@
+package com.prisonerprice.reactiveMongoTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoTestingApplication.class, args);
+	}
+
+}
---
+++ reactiveMongoTesting/src/test/java/com/prisonerprice/reactiveMongoTesting/ReactiveMongoTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisonerprice.reactiveMongoTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMongoTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
