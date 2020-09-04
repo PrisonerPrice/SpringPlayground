3354635d0f3d3b5f45fbda7492d4a9ff017db1a6
Hangbo <zhzhanghangbo@hotmail.com>
2020-09-04 00:38:02 -0400

    Start working with Spring and Tesseract

---
+++ SpringTesseract/src/main/java/com/prisonerprice/SpringTesseract/SpringTesseractApplication.java
@@ -0,0 +1,13 @@
+package com.prisonerprice.SpringTesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTesseractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTesseractApplication.class, args);
+	}
+
+}
---
+++ SpringTesseract/src/test/java/com/prisonerprice/SpringTesseract/SpringTesseractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisonerprice.SpringTesseract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTesseractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
