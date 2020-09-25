b4463037ba3953dc2ed81ed3f97d39df4d3d57d9
Hangbo <zhzhanghangbo@hotmail.com>
2020-09-25 17:27:43 -0400

    Add SimpleKafka

---
+++ SimpleKafka/src/test/java/com/prisonerprice/SimpleKafka/SimpleKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisonerprice.SimpleKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
